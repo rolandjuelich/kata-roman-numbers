80843066dbc2c9f4596299bec9e9948e50753bec
Roland Jülich <rolandjuelich@gmail.com>
2018-10-30 19:20:06 +0100

    initial checkin

---
+++ src/test/java/my/kata/romannumerals/RomanNumeralsTest.java
@@ -0,0 +1,14 @@
+package my.kata.romannumerals;
+
+import static org.assertj.core.api.Assertions.fail;
+
+import org.junit.Test;
+
+public class RomanNumeralsTest {
+
+	@Test
+	public void shouldDoSth() {
+		fail("here could be your first test");
+	}
+	
+}
