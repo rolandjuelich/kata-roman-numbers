b9e7d8f4a660aebfa30f937aaccd0ddf6a1ed542
Roland Jülich <rolandjuelich@gmail.com>
2018-11-23 12:01:30 +0100

    refactor from trivial solution

--- src/test/java/my/kata/number/roman/RomanNumberUnitTest.java
+++ src/test/java/my/kata/number/roman/RomanNumberUnitTest.java
@@ -1,14 +1,28 @@
 package my.kata.number.roman;
 
-import static org.assertj.core.api.Assertions.fail;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.approvaltests.Approvals;
 import org.junit.Test;
 
+/**
+ * @see <a href=
+ *      "https://github.com/approvals/ApprovalTests.Java#examples">ApprovalTests
+ *      on Github</a>
+ */
 public class RomanNumberUnitTest {
 
 	@Test
-	public void shouldDoSth() {
-		fail("here could be your first test");
+	public void shouldConvertArabicToRomanNumber() {
+
+		final List<String> romanNumbers = new ArrayList<String>();
+
+		for (int i = 0; i <= 3000; i++) {
+			romanNumbers.add(RomanNumber.convert(i));
+		}
+
+		Approvals.verifyAll("", romanNumbers);
 	}
-	
+
 }
