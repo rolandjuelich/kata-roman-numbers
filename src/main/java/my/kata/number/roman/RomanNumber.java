ad0d7a8af7ef35d24bc5e62fd7a6f7406541e285
Roland Jülich <rolandjuelich@gmail.com>
2018-11-23 15:00:53 +0100

    refactor

--- src/main/java/my/kata/number/roman/RomanNumber.java
+++ src/main/java/my/kata/number/roman/RomanNumber.java
@@ -29,20 +29,19 @@ public class RomanNumber {
 			return ciphers.get(1);
 		}
 
-		if (number <= 5) {
-			if (number < 5 - 1) {
-				return convert(1) + convert(number - 1);
-			}
-			if (number == 5 - 1) {
-				return convert(1) + convert(5);
-			}
-			if (number == 5) {
-				return ciphers.get(5);
-			}
-			return convert(5 - 1) + convert(number - (5 - 1));
-		}
-
 		if (number <= 10) {
+			if (number <= 5) {
+				if (number < 5 - 1) {
+					return convert(1) + convert(number - 1);
+				}
+				if (number == 5 - 1) {
+					return convert(1) + convert(5);
+				}
+				if (number == 5) {
+					return ciphers.get(5);
+				}
+				return convert(5 - 1) + convert(number - (5 - 1));
+			}
 			if (number < 10 - 1) {
 				return convert(5) + convert(number - 5);
 			}
@@ -55,20 +54,19 @@ public class RomanNumber {
 			return convert(10 - 1) + convert(number - (10 - 1));
 		}
 
-		if (number <= 50) {
-			if (number < 50 - 10) {
-				return convert(10) + convert(number - 10);
-			}
-			if (number == 50 - 10) {
-				return convert(10) + convert(50);
-			}
-			if (number == 50) {
-				return ciphers.get(50);
-			}
-			return convert(50 - 10) + convert(number - (50 - 10));
-		}
-
 		if (number <= 100) {
+			if (number <= 50) {
+				if (number < 50 - 10) {
+					return convert(10) + convert(number - 10);
+				}
+				if (number == 50 - 10) {
+					return convert(10) + convert(50);
+				}
+				if (number == 50) {
+					return ciphers.get(50);
+				}
+				return convert(50 - 10) + convert(number - (50 - 10));
+			}
 			if (number < 100 - 10) {
 				return convert(50) + convert(number - 50);
 			}
@@ -81,20 +79,19 @@ public class RomanNumber {
 			return convert(100 - 10) + convert(number - (100 - 10));
 		}
 
-		if (number <= 500) {
-			if (number < 500 - 100) {
-				return convert(100) + convert(number - 100);
-			}
-			if (number == 500 - 100) {
-				return convert(100) + convert(500);
-			}
-			if (number == 500) {
-				return ciphers.get(500);
-			}
-			return convert(500 - 100) + convert(number - (500 - 100));
-		}
-
 		if (number <= 1000) {
+			if (number <= 500) {
+				if (number < 500 - 100) {
+					return convert(100) + convert(number - 100);
+				}
+				if (number == 500 - 100) {
+					return convert(100) + convert(500);
+				}
+				if (number == 500) {
+					return ciphers.get(500);
+				}
+				return convert(500 - 100) + convert(number - (500 - 100));
+			}
 			if (number < 1000 - 100) {
 				return convert(500) + convert(number - 500);
 			}
