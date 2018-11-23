de1ae63aad74deb7754f8e479a5018e17d176133
Roland Jülich <rolandjuelich@gmail.com>
2018-11-23 14:52:28 +0100

    refactor 0-3000

--- src/main/java/my/kata/number/roman/RomanNumber.java
+++ src/main/java/my/kata/number/roman/RomanNumber.java
@@ -10,10 +10,6 @@ public class RomanNumber {
 
 	public static String convert(int number) {
 
-		if (number < 1) {
-			return null;
-		}
-
 		TreeMap<Integer, String> ciphers = new TreeMap<Integer, String>();
 		ciphers.put(1, "I");
 		ciphers.put(5, "V");
@@ -26,95 +22,92 @@ public class RomanNumber {
 		List<Integer> numbers = Lists.newArrayList(ciphers.keySet());
 		Collections.reverse(numbers);
 
-		if (number < 20) {
-			for (Integer arabic : numbers) {
-				if (number >= arabic - 1) {
-					if (number < arabic) {
-						return convert(1) + convert(arabic);
-					}
-					if (number == arabic) {
-						return ciphers.get(arabic);
-					}
-					return convert(arabic) + convert(number - arabic);
-				}
+		if (number <= 1) {
+			if (number < 1) {
+				return null;
 			}
+			return ciphers.get(1);
 		}
 
-		if (number < 40) {
-			for (Integer arabic : numbers) {
-				if (number >= arabic - 10) {
-					if (number < arabic) {
-						return convert(10) + convert(arabic);
-					}
-					if (number == arabic) {
-						return ciphers.get(arabic);
-					}
-					return convert(arabic) + convert(number - arabic);
-				}
+		if (number <= 5) {
+			if (number < 5 - 1) {
+				return convert(1) + convert(number - 1);
 			}
+			if (number == 5 - 1) {
+				return convert(1) + convert(5);
+			}
+			if (number == 5) {
+				return ciphers.get(5);
+			}
+			return convert(5 - 1) + convert(number - (5 - 1));
 		}
 
-		if (number < 90) {
-			if (number < 50) {
-				if (number == 50 - 10) {
-					return convert(10) + convert(50);
-				}
-				if (number > (50 - 10)) {
-					return convert(50 - 10) + convert(number - (50 - 10));
-				}
+		if (number <= 10) {
+			if (number < 10 - 1) {
+				return convert(5) + convert(number - 5);
+			}
+			if (number == 10 - 1) {
+				return convert(1) + convert(10);
+			}
+			if (number == 10) {
+				return ciphers.get(10);
+			}
+			return convert(10 - 1) + convert(number - (10 - 1));
+		}
+
+		if (number <= 50) {
+			if (number < 50 - 10) {
+				return convert(10) + convert(number - 10);
+			}
+			if (number == 50 - 10) {
+				return convert(10) + convert(50);
 			}
 			if (number == 50) {
 				return ciphers.get(50);
 			}
-			if (number > 50) {
-				return convert(50) + convert(number - 50);
-			}
+			return convert(50 - 10) + convert(number - (50 - 10));
 		}
 
 		if (number <= 100) {
-			if (number == 90) {
+			if (number < 100 - 10) {
+				return convert(50) + convert(number - 50);
+			}
+			if (number == 100 - 10) {
 				return convert(10) + convert(100);
 			}
 			if (number == 100) {
 				return ciphers.get(100);
 			}
-			return convert(90) + convert(number - 90);
+			return convert(100 - 10) + convert(number - (100 - 10));
 		}
 
-		if (number <= 400) {
-			if (number == 400) {
+		if (number <= 500) {
+			if (number < 500 - 100) {
+				return convert(100) + convert(number - 100);
+			}
+			if (number == 500 - 100) {
 				return convert(100) + convert(500);
 			}
-			return convert(100) + convert(number - 100);
+			if (number == 500) {
+				return ciphers.get(500);
+			}
+			return convert(500 - 100) + convert(number - (500 - 100));
 		}
 
-		if (number < 500) {
-			return convert(400) + convert(number - 400);
+		if (number <= 1000) {
+			if (number < 1000 - 100) {
+				return convert(500) + convert(number - 500);
+			}
+			if (number == 1000 - 100) {
+				return convert(100) + convert(1000);
+			}
+			if (number == 1000) {
+				return ciphers.get(1000);
+			}
+			return convert(1000 - 100) + convert(number - (1000 - 100));
 		}
 
-		if (number == 500) {
-			return ciphers.get(500);
-		}
-
-		if (number < 900) {
-			return convert(500) + convert(number - 500);
-		}
-
-		if (number == 900) {
-			return convert(100) + convert(1000);
-		}
-
-		if (number == 1000) {
-			return ciphers.get(1000);
-		}
-		if (number < 1000) {
-			return convert(900) + convert(number - 900);
-		}
-		if (number > 1000) {
-			return convert(1000) + convert(number - 1000);
-		}
-
-		return null;
+		return convert(1000) + convert(number - 1000);
 	}
 
 }
