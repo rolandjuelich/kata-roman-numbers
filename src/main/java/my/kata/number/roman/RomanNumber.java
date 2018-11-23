17e79202f8659273579dd73022c39dda612b36e8
Roland Jülich <rolandjuelich@gmail.com>
2018-11-23 15:01:56 +0100

    refactor

--- src/main/java/my/kata/number/roman/RomanNumber.java
+++ src/main/java/my/kata/number/roman/RomanNumber.java
@@ -30,20 +30,20 @@ public class RomanNumber {
 		}
 
 		if (number <= 10) {
-			if (number <= 5) {
-				if (number < 5 - 1) {
+			if (number <= (10 / 2)) {
+				if (number < (10 / 2) - 1) {
 					return convert(1) + convert(number - 1);
 				}
-				if (number == 5 - 1) {
-					return convert(1) + convert(5);
+				if (number == (10 / 2) - 1) {
+					return convert(1) + convert((10 / 2));
 				}
-				if (number == 5) {
-					return ciphers.get(5);
+				if (number == (10 / 2)) {
+					return ciphers.get((10 / 2));
 				}
-				return convert(5 - 1) + convert(number - (5 - 1));
+				return convert((10 / 2) - 1) + convert(number - ((10 / 2) - 1));
 			}
 			if (number < 10 - 1) {
-				return convert(5) + convert(number - 5);
+				return convert((10 / 2)) + convert(number - (10 / 2));
 			}
 			if (number == 10 - 1) {
 				return convert(1) + convert(10);
@@ -55,20 +55,20 @@ public class RomanNumber {
 		}
 
 		if (number <= 100) {
-			if (number <= 50) {
-				if (number < 50 - 10) {
+			if (number <= (100 / 2)) {
+				if (number < (100 / 2) - 10) {
 					return convert(10) + convert(number - 10);
 				}
-				if (number == 50 - 10) {
-					return convert(10) + convert(50);
+				if (number == (100 / 2) - 10) {
+					return convert(10) + convert((100 / 2));
 				}
-				if (number == 50) {
-					return ciphers.get(50);
+				if (number == (100 / 2)) {
+					return ciphers.get((100 / 2));
 				}
-				return convert(50 - 10) + convert(number - (50 - 10));
+				return convert((100 / 2) - 10) + convert(number - ((100 / 2) - 10));
 			}
 			if (number < 100 - 10) {
-				return convert(50) + convert(number - 50);
+				return convert((100 / 2)) + convert(number - (100 / 2));
 			}
 			if (number == 100 - 10) {
 				return convert(10) + convert(100);
@@ -80,20 +80,20 @@ public class RomanNumber {
 		}
 
 		if (number <= 1000) {
-			if (number <= 500) {
-				if (number < 500 - 100) {
+			if (number <= (1000 / 2)) {
+				if (number < (1000 / 2) - 100) {
 					return convert(100) + convert(number - 100);
 				}
-				if (number == 500 - 100) {
-					return convert(100) + convert(500);
+				if (number == (1000 / 2) - 100) {
+					return convert(100) + convert((1000 / 2));
 				}
-				if (number == 500) {
-					return ciphers.get(500);
+				if (number == (1000 / 2)) {
+					return ciphers.get((1000 / 2));
 				}
-				return convert(500 - 100) + convert(number - (500 - 100));
+				return convert((1000 / 2) - 100) + convert(number - ((1000 / 2) - 100));
 			}
 			if (number < 1000 - 100) {
-				return convert(500) + convert(number - 500);
+				return convert((1000 / 2)) + convert(number - (1000 / 2));
 			}
 			if (number == 1000 - 100) {
 				return convert(100) + convert(1000);
