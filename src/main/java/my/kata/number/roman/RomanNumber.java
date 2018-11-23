699b3979fd0ea512e8e8bc3e327f474a4be81fc0
Roland Jülich <rolandjuelich@gmail.com>
2018-11-23 15:12:33 +0100

    refactor

--- src/main/java/my/kata/number/roman/RomanNumber.java
+++ src/main/java/my/kata/number/roman/RomanNumber.java
@@ -6,20 +6,11 @@ public class RomanNumber {
 
 	public static String convert(int number) {
 
-		TreeMap<Integer, String> ciphers = new TreeMap<Integer, String>();
-		ciphers.put(1, "I");
-		ciphers.put(5, "V");
-		ciphers.put(10, "X");
-		ciphers.put(50, "L");
-		ciphers.put(100, "C");
-		ciphers.put(500, "D");
-		ciphers.put(1000, "M");
-
 		if (number <= 1) {
 			if (number < 1) {
 				return null;
 			}
-			return ciphers.get(1);
+			return ciphers().get(1);
 		}
 
 		for (int i = 10; i <= 1000; i *= 10) {
@@ -34,7 +25,7 @@ public class RomanNumber {
 					return convert(m) + convert(n);
 				}
 				if (number == n) {
-					return ciphers.get(n);
+					return ciphers().get(n);
 				}
 				return convert(n - m) + convert(number - (n - m));
 			}
@@ -46,13 +37,25 @@ public class RomanNumber {
 					return convert(m) + convert(i);
 				}
 				if (number == i) {
-					return ciphers.get(i);
+					return ciphers().get(i);
 				}
 				return convert(i - m) + convert(number - (i - m));
 			}
 		}
 
 		return convert(1000) + convert(number - 1000);
+	}
+
+	private static TreeMap<Integer, String> ciphers() {
+		TreeMap<Integer, String> ciphers = new TreeMap<Integer, String>();
+		ciphers.put(1, "I");
+		ciphers.put(5, "V");
+		ciphers.put(10, "X");
+		ciphers.put(50, "L");
+		ciphers.put(100, "C");
+		ciphers.put(500, "D");
+		ciphers.put(1000, "M");
+		return ciphers;
 	}
 
 }
