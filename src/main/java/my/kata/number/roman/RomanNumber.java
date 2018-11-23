d145122a5bc2388b0f9215f331ea43b39fdb09a3
Roland Jülich <rolandjuelich@gmail.com>
2018-11-23 15:06:06 +0100

    refactor

--- src/main/java/my/kata/number/roman/RomanNumber.java
+++ src/main/java/my/kata/number/roman/RomanNumber.java
@@ -49,56 +49,6 @@ public class RomanNumber {
 			}
 		}
 
-		if (number <= 100) {
-			if (number <= (100 / 2)) {
-				if (number < (100 / 2) - 10) {
-					return convert(10) + convert(number - 10);
-				}
-				if (number == (100 / 2) - 10) {
-					return convert(10) + convert((100 / 2));
-				}
-				if (number == (100 / 2)) {
-					return ciphers.get((100 / 2));
-				}
-				return convert((100 / 2) - 10) + convert(number - ((100 / 2) - 10));
-			}
-			if (number < 100 - 10) {
-				return convert((100 / 2)) + convert(number - (100 / 2));
-			}
-			if (number == 100 - 10) {
-				return convert(10) + convert(100);
-			}
-			if (number == 100) {
-				return ciphers.get(100);
-			}
-			return convert(100 - 10) + convert(number - (100 - 10));
-		}
-
-		if (number <= 1000) {
-			if (number <= (1000 / 2)) {
-				if (number < (1000 / 2) - 100) {
-					return convert(100) + convert(number - 100);
-				}
-				if (number == (1000 / 2) - 100) {
-					return convert(100) + convert((1000 / 2));
-				}
-				if (number == (1000 / 2)) {
-					return ciphers.get((1000 / 2));
-				}
-				return convert((1000 / 2) - 100) + convert(number - ((1000 / 2) - 100));
-			}
-			if (number < 1000 - 100) {
-				return convert((1000 / 2)) + convert(number - (1000 / 2));
-			}
-			if (number == 1000 - 100) {
-				return convert(100) + convert(1000);
-			}
-			if (number == 1000) {
-				return ciphers.get(1000);
-			}
-			return convert(1000 - 100) + convert(number - (1000 - 100));
-		}
-
 		return convert(1000) + convert(number - 1000);
 	}
 
